a638009afa66488b5c65e5bc7ae86fb42d80e973
Robert Starmer <rstarmer@gmail.com>
2015-02-26 22:25:48 -0700

    Remove Lab2

--- swift_temp.sh
+++ swift_temp.sh
@@ -1,30 +1,26 @@
 #!/bin/bash
 #swift_temp.sh
 
-echo >> class_test_file.txt <<EOF
+cat >> class_test_file.txt <<EOF
 This is a simple test file for Swift to validate
 that ${OS_USERNAME} was able to grab a document
 from a temporary URL.
 EOF
-swift upload lab_container class_test_file --header "X-Delete-After:40"
-swift post -m "Temp-URL-Key:my_own_secret"
 
-temp_url=swift tempurl GET 35 /v1/KEY_${OS_TENANT_ID}/lab_container/class_test_file.txt my_own_secret
-wget -O - "https://dnvrco-api.os.cloud.twc.net${temp_url}"
+swift upload lab_container class_test_file.txt  --header X-Delete-After:60
+swift post -m "Temp-URL-Key:my_own_secret"
+temp_url=`swift tempurl GET 100 /v1/KEY_${OS_TENANT_ID}/lab_container/class_test_file.txt my_own_secret`
+wget -O - "https://chrcnc-api.os.cloud.twc.net${temp_url}"
 rm class_test_file.txt
 echo ""
-echo "sleeping for 15 seconds"
-sleep 15
+echo "sleeping for 45 seconds"
+sleep 45
+echo "first test"
 swift list lab_container
-wget -O - "https://dnvrco-api.os.cloud.twc.net${temp_url}"
-echo "sleeping for 15 seconds"
-sleep 15
+wget -O - "https://chrcnc-api.os.cloud.twc.net${temp_url}"
+echo "sleeping for another 45 seconds, container should be empty"
+sleep 45
 swift list lab_container
-wget -O - "https://dnvrco-api.os.cloud.twc.net${temp_url}"
-
-echo "sleeping for 15 seconds"
-sleep 15
-swift list lab_container
-wget -O - "https://dnvrco-api.os.cloud.twc.net${temp_url}"
+wget -O - "https://chrcnc-api.os.cloud.twc.net${temp_url}"
 
 exit 0
