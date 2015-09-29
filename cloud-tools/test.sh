a0a62ae4d9938eeeef948486daf6d6b34a1a3227
Robert Starmer <rstarmer@gmail.com>
2015-09-29 10:41:39 -0700

    Add floating ip to test.sh

--- cloud-tools/test.sh
+++ cloud-tools/test.sh
@@ -2,7 +2,7 @@
 
 vmname=${1:-admin}
 keyname=${2:-class2}
-floatingip=${3:-10.1.64.32}
+#floatingip=${3:-10.1.64.32}
 
 cat > data.txt <<EOF
 #!/bin/bash
@@ -18,10 +18,10 @@ EOF
 python -m SimpleHTTPServer 80
 EOF
 
+floatingip=`nova floating-ip-create sixtyfour | awk '/sixtyfour/ {print $2}'`
 nova boot ${vmname} --image trusty --flavor m1.small --key-name ${keyname} --nic net-id=47ea0f46-5728-4a22-bab7-1417a21539fd --config-drive True --user-data ./data.txt
-nova floating-ip-associate admin ${floatingip}
+nova floating-ip-associate ${vmname} ${floatingip}
 
-test=''
 until [[ $(wget -qO - http://${floatingip}/test.htm) =~ done ]]
  do echo not done
  sleep 5
@@ -30,3 +30,4 @@ done
 echo "Test is $(wget -qO - http://${floatingip}/test.htm)"
 
 nova delete ${vmname}
+nova floating-ip-delete ${floatingip}
