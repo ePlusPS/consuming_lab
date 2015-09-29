df6abf0e361e32414b7643aa38f043be463e56a8
Robert Starmer <rstarmer@gmail.com>
2015-09-29 10:56:42 -0700

    Update network id

--- cloud-tools/test.sh
+++ cloud-tools/test.sh
@@ -2,8 +2,7 @@
 
 vmname=${1:-admin}
 keyname=${2:-class2}
-#floatingip=${3:-10.1.64.32}
-
+netname=${3:-private}
 cat > data.txt <<EOD
 #!/bin/bash
 passwd ubuntu <<EOF
@@ -19,7 +18,8 @@ python -m SimpleHTTPServer 80
 EOD
 
 floatingip=`nova floating-ip-create sixtyfour | awk '/sixtyfour/ {print $2}'`
-nova boot ${vmname} --image trusty --flavor m1.small --key-name ${keyname} --nic net-id=47ea0f46-5728-4a22-bab7-1417a21539fd --config-drive True --user-data ./data.txt
+net_id=`neutron net-list | grep ${netname} | awk '/ | / {print $2}'`
+nova boot ${vmname} --image trusty --flavor m1.small --key-name ${keyname} --nic net-id=${net_id} --config-drive True --user-data ./data.txt
 nova floating-ip-associate ${vmname} ${floatingip}
 
 until [[ $(wget -qO - http://${floatingip}/test.htm) =~ done ]]
